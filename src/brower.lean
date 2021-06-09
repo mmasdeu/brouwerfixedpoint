8045a57ae7e5856eeb88de2277a2c35d430d79e6
Eloi <eloitorrentsj@gmail.com>
2021-06-09 21:18:32 +0200

    started definition diameter

--- src/brower.lean
+++ src/brower.lean
@@ -10,4 +10,11 @@ local notation `E` := fin m → ℝ
 -- The proposition that a given sperner colouring is sperner.
 #check is_sperner_colouring
 
+open affine
+
+def diameter (S: simplicial_complex E): ℝ :=
+ sorry
+ -- maxima distancia entre dos punts qualsevols
+ -- si no hi hagues maxim (un complexe simplicial infinit), que fem?
+
 
