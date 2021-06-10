82e86ed024cb89b88b0b9d5217ff708bb2d22efb
Eloi <eloitorrentsj@gmail.com>
2021-06-10 11:30:50 +0200

    Changed repository structure

--- src/brower.lean
+++ src/brower.lean
@@ -1,18 +1,20 @@
 import data.real.basic
+import combinatorics.simplicial_complex.basic
 import combinatorics.simplicial_complex.sperner
+
 
 variables {m n : ℕ}
 local notation `E` := fin m → ℝ
+
 
 open affine
 
 -- The proposition that a given sperner colouring is sperner.
 #check is_sperner_colouring
 
-
-def diameter (S: simplicial_complex E) : ℝ :=
+def diameter (S: simplicial_complex E): ℝ :=
  sorry
- -- maxima distancia entre dos punts qualssevol
+ -- maxima distancia entre dos punts qualsevols
  -- si no hi hagues maxim (un complexe simplicial infinit), que fem?
 
 
