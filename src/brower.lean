b97d72523bba9ea853c5afe3df25d796a768b77e
Marc Masdeu <marc.masdeu@gmail.com>
2021-06-10 11:21:53 +0200

    Changed imports to use mathlib instead of dependencies.

--- src/brower.lean
+++ src/brower.lean
@@ -1,20 +1,18 @@
-import algebraic_topology.simplicial_object
 import data.real.basic
-import .dependencies.simplicial_complex
-import .dependencies.sperner
-
+import combinatorics.simplicial_complex.sperner
 
 variables {m n : ℕ}
 local notation `E` := fin m → ℝ
 
+open affine
+
 -- The proposition that a given sperner colouring is sperner.
 #check is_sperner_colouring
 
-open affine
 
-def diameter (S: simplicial_complex E): ℝ :=
+def diameter (S: simplicial_complex E) : ℝ :=
  sorry
- -- maxima distancia entre dos punts qualsevols
+ -- maxima distancia entre dos punts qualssevol
  -- si no hi hagues maxim (un complexe simplicial infinit), que fem?
 
 
