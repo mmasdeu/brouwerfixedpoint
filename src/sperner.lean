7d6b53ac8146786251bbf129850933c055176582
Carlos Caralps Rueda <carlosca2001@gmail.com>
2021-06-22 00:13:23 +0200

    Proof diameter_growth, added new characterisation

--- src/sperner.lean
+++ src/sperner.lean
@@ -36,10 +36,7 @@ lemma fixed_point_of_epsilon_fixed (X : Type) [metric_space X]
   (h : ∀ (ε : ℝ), 0 < ε → ∃ x, dist x (f x) < ε) :
   ∃ x : X, f x = x :=
 begin
-  have hpos : ∀ (n : ℕ), 0 < 1 / ((n+1) : ℝ),
-  {
-    apply nat.one_div_pos_of_nat,
-  },
+  have hpos : ∀ (n : ℕ), 0 < 1 / ((n+1) : ℝ), by apply nat.one_div_pos_of_nat,
   let a : ℕ → X := λ n, classical.some (h (1 / ((n+1) : ℝ)) (hpos n)),
   have ha : ∀ n, dist (a n) (f (a n)) < 1 / ((n+1) : ℝ) :=
     λ n, classical.some_spec (h (1 / ((n+1) : ℝ)) (hpos n)),
@@ -61,13 +58,9 @@ begin
   {
     intros δ hδ,
     rw seq_tendsto_iff at hΦ2,
-    specialize hΦ2 (metric.ball z (δ)) (_) (metric.is_open_ball),
-    {
-      rw [metric.mem_ball, dist_self],
-      linarith,
-    },
+    specialize hΦ2 (metric.ball z (δ)) (by rwa [metric.mem_ball, dist_self]) (metric.is_open_ball),
     simp [metric.mem_ball, dist_comm] at hΦ2,
-    simp [←dist_comm],
+    simp only [function.comp_app],
     exact hΦ2,
   },
   have H2 : ∃ (n : ℕ), ∀ m ≥ n, dist ((a∘Φ) m) (f ((a∘Φ) m)) ≤ ε/3,
@@ -86,10 +79,7 @@ begin
     use n,
     intros m hm,
     specialize ha (Φ m),
-    have hmn : 1 / ((m + 1) : ℝ) ≤ 1 / ((n + 1) : ℝ),
-    {
-      apply nat.one_div_le_one_div hm,
-    },
+    have hmn : 1 / ((m + 1) : ℝ) ≤ 1 / ((n + 1) : ℝ), by apply nat.one_div_le_one_div hm,
     have hinc : 1 / ((Φ m) + 1:ℝ) ≤ 1 / ((m + 1):ℝ),
     {
       have h' : m ≤ Φ m := strict_mono.id_le hΦ1 m,
@@ -97,45 +87,44 @@ begin
     },
     linarith,
   },
-  have H3 : ∃ (n : ℕ), ∀ m ≥ n, dist (f ((a∘Φ) m)) (f z) < ε/3,
-  {
-    rw metric.continuous_iff at hf,
-    specialize hf z (ε/3) (by linarith),
-    obtain ⟨δ, ⟨hδpos, h'⟩⟩ := hf,
-    obtain ⟨n1, hn1⟩ := H1 δ hδpos,
-    use n1,
-    intros m hm,
-    specialize hn1 m hm,
-    rw dist_comm at hn1,
-    specialize h' (a (Φ m)) hn1,
-    exact h',
-  },
-  obtain ⟨n1, hn1⟩ := H1 (ε / 3) (by linarith),
-  obtain ⟨n2, hn2⟩ := H2,
-  obtain ⟨n3, hn3⟩ := H3,
+  have H3 : ∃ (n : ℕ), ∀ m ≥ n, dist (f ((a∘Φ) m)) (f z) < ε/3 := 
+      let ⟨δ, ⟨hδpos, h'⟩⟩ := (metric.continuous_iff.1 hf) z (ε/3) (by linarith), ⟨n1, hn1⟩ := H1 δ hδpos in 
+        ⟨n1, λ m hm, let h := hn1 m hm in h' (a (Φ m)) (by rwa dist_comm)⟩,
+  obtain ⟨⟨n1, hn1⟩, ⟨n2, hn2⟩, ⟨n3, hn3⟩⟩ := ⟨H1 (ε / 3) (by linarith), H2, H3⟩,
   let n := max (max n1 n2) n3,
-  specialize hn1 n _,
-  {
-    calc n ≥ max n1 n2 : by {exact le_max_left (max n1 n2) n3}
-    ... ≥ n1 : by {exact le_max_left n1 n2}
-  },
-  specialize hn2 n _,
-  {
-    calc n ≥ max n1 n2 : by {exact le_max_left (max n1 n2) n3}
-    ... ≥ n2 : by {exact le_max_right n1 n2}
-  },
+  specialize hn1 n (le_of_max_le_left (le_max_left (max n1 n2) n3)),
+  specialize hn2 n (le_trans (le_max_right n1 n2) (le_max_left (max n1 n2) n3)),
   specialize hn3 n (le_max_right (max n1 n2) n3),
   calc
   dist z (f z) ≤ dist z ((a ∘ Φ) n)
                 + dist ((a ∘ Φ) n) (f ((a ∘ Φ) n))
                 + dist (f ((a ∘ Φ) n)) (f z) : dist_triangle4 z ((a ∘ Φ) n) (f ((a ∘ Φ) n)) (f z)
   ... ≤ ε/3 + ε/3 + ε/3 : by { linarith [hn1, hn2, hn3] }
-  ... = ε : by {linarith}
+  ... = ε : by {ring},
 end
 
-lemma diameter_growth (X : Type) [metric_space X] (S : set X)
-(f : X → X) (hf : uniform_continuous_on f S) (ε : ℝ) (hε : 0 < ε)
-: ∃ δ > 0, ∀ T ⊆ S, metric.diam T < δ → metric.diam (f '' T) < ε :=
+lemma characterisation_uniform_continuous_on {X : Type} [metric_space X] (S : set X) (f : X → X):
+  uniform_continuous_on f S ↔ ∀ (ε : nnreal) (hε : ε > 0), ∃ (δ : nnreal) (hδ : δ > 0), 
+  ∀ (x y : X), edist x y ≤ δ → edist (f x) (f y) ≤ ε :=
 begin
   sorry
 end
+
+lemma enndiameter_growth (X : Type) [metric_space X] (S : set X)
+(f : X → X) (hf : uniform_continuous_on f S) (ε : nnreal) (hε : 0 < ε) : 
+  ∃ (δ : nnreal) (hδ : δ > 0), ∀ T ⊆ S, emetric.diam T ≤ δ → emetric.diam (f '' T) ≤ ε :=
+let ⟨δ, hδ, H⟩ := (characterisation_uniform_continuous_on S f).1 hf ε hε in 
+  ⟨δ, hδ, (λ R hR hdR, emetric.diam_image_le_iff.2 (λ x hx y hy, H x y (emetric.diam_le_iff.1 hdR x hx y hy)))⟩
+
+lemma diameter_growth (X : Type) [metric_space X] (S : set X)
+(f : X → X) (hf : uniform_continuous_on f S) (ε : ℝ) (hε : 0 < ε) : 
+  ∃ δ > 0, ∀ T ⊆ S, metric.bounded T → metric.diam T ≤ δ → metric.diam (f '' T) ≤ ε :=
+let ⟨δ, ⟨hδ,HH⟩⟩ := enndiameter_growth _ _ _ hf _ (real.to_nnreal_pos.mpr hε),
+h2 : ∀ (T : set X), T ⊆ S → metric.bounded T → metric.diam T ≤ ↑δ → metric.diam (f '' T) ≤ ε :=
+  (λ T hT1 hTb hT2, let hT2 : emetric.diam T ≤ δ := 
+    (let htop := metric.bounded.ediam_ne_top hTb in
+      (ennreal.to_real_le_to_real htop (ennreal.coe_ne_top)).mp (by rwa ennreal.coe_to_real)), 
+    hHH := HH T hT1 hT2 in 
+    by rwa [metric.diam, ←@ennreal.le_of_real_iff_to_real_le (emetric.diam (f '' T)) ε 
+      (λ hc, ennreal.not_top_le_coe (by rwa ← hc)) (le_of_lt hε)]) in 
+⟨δ, nnreal.coe_pos.mpr hδ, h2⟩
