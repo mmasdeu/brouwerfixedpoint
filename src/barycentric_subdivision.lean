f9323f10aab50755d8f6c8d1ea8803129465412f
Eloi Torrents <eloitorrentsj@gmail.com>
2021-09-08 00:15:46 +0200

    Progress in barycentric subdivisions

--- src/barycentric_subdivision.lean
+++ src/barycentric_subdivision.lean
@@ -11,69 +11,36 @@ noncomputable def barycenter(L: list E): E :=
   
 variables (L : list E) [pseudo_metric_space E]
 
--- Why can't I turn this into an instance?
-lemma tail_nodup {T: Type}(L : list T) [list.nodup L] : list.nodup (L.tail) :=
-begin
-  induction L with x L IH,
-  {
-    simp,
-  },
-  {
-    simp,
-    finish,
-  },
-end
-
-lemma tail_nodup2 {T: Type}(head: T)(tail : list T) [list.nodup (head :: tail)] : list.nodup tail :=
-begin
-  induction tail with x tail IH,
-  {
-    simp,
-  },
-  {
-    simp,
-    finish,
-  },
-end
-
 namespace list
 
 -- Given an order of the vertices of each face, we have a simplex of the barycentric subdivision.
--- To do, add `[list.nodup L]`. Done, but I'm not sure if it's useful.
-def face_baricentric_subdivision{L : list E}[nodup: list.nodup L] : set E :=
-  begin
-    induction L with head tail smaller_dim_baricentric_subdivision nodup,
-    { exact (∅ : finset E) },
-    { 
-      have : tail.nodup :=  @tail_nodup2 _ _ _ nodup,
-      exact {barycenter (list.cons head tail)} ∪ (@smaller_dim_baricentric_subdivision this)},
-    
-    -- Recursive definition
-    -- If L.length > 1,
-    -- add the point `barycenter L` and all the points of `face_barycentric_subdivision L.tail`
-    -- If L.length = 1, add the point.
+  -- Recursive definition
+  -- If L.length > 1,
+  -- add the point `barycenter L` and all the points of `face_barycentric_subdivision L.tail`
+  -- If L.length = 1, add the point.
 
-  end
-
-def face_baricentric_subdivision2{L : list E} : set E :=
+noncomputable def face_baricentric_subdivision(L : list E) : finset E :=
   begin
     induction L with head tail smaller_dim_baricentric_subdivision,
     { exact (∅ : finset E) },
-    { 
-      exact { barycenter (list.cons head tail) } ∪ smaller_dim_baricentric_subdivision },
-    
-    -- Recursive definition
-    -- If L.length > 1,
-    -- add the point `barycenter L` and all the points of `face_barycentric_subdivision L.tail`
-    -- If L.length = 1, add the point.
+    { exact { barycenter (list.cons head tail) } ∪ smaller_dim_baricentric_subdivision },
   end
+   
+open affine
 
 -- The goal is to complete this definition.
-/-def simplicial_complex.barycentric_subdivision (S : simplicial_complex E) : simplicial_complex E :=
-{ faces := {X | ∃ {L : list (fin m → ℝ)}, list.to_finset L ∈ S.faces ∧ X = },
-  indep := _,
-  down_closed := _,
-  disjoint := _ }-/
+noncomputable def simplicial_complex.barycentric_subdivision (S : simplicial_complex E) : simplicial_complex E :=
+  { faces := {X | ∃ {L : list E}, list.nodup L ∧ list.to_finset L ∈ S.faces ∧ X = face_baricentric_subdivision L},
+  indep := 
+    begin
+      rintros face ⟨vertex_list, ⟨vertex_nodup, ⟨h_face3, face_from_vertex_list⟩⟩⟩,
+      cases S,
+      simp at h_face3,
+      -- help?
+      sorry,
+    end,
+  down_closed := sorry,
+  disjoint := sorry }
 
 -- The maximum distance bewteen any pair of vertices in a simplicial complex.
 noncomputable def distance_vertices  (A : set E) : ℝ :=
